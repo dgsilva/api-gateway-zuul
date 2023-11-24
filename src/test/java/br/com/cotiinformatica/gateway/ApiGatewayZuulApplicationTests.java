ce0338f36873241cffad2ada62199135a7b1e30a
Diego Bizerra da Silva <diegorj93@gmail.com>
2023-11-24 16:37:46 -0300

    Primeiro commit

---
+++ src/test/java/br/com/cotiinformatica/gateway/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
